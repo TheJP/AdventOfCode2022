2c3b45bbaaff9c5b0d6cb282d5bb8a4a261dfc2f
TheJP <jp_55555@hotmail.com>
2022-12-05 19:26:17 +0100

    Solved Day 3 Part 2

---
+++ day03.2/Program.cs
@@ -0,0 +1,35 @@
+﻿using var input = new StreamReader(Environment.GetCommandLineArgs()[1]);
+long score = 0;
+string? line;
+var lines = new List<string>();
+while ((line = input.ReadLine()) != null)
+{
+    if (string.IsNullOrWhiteSpace(line)) return;
+    lines.Add(line);
+}
+
+foreach (var group in lines.Chunk(3)) {
+    if (group.Length != 3) throw new InvalidOperationException();
+
+    var a = group[0].ToArray();
+    var b = group[1].ToArray();
+    var c = group[2].ToArray();
+    Array.Sort(a);
+    Array.Sort(b);
+    Array.Sort(c);
+
+    var (u, v, w) = (0, 0, 0);
+    while (a[u] != b[v] || b[v] != c[w]) {
+        if (a[u] < b[v]) ++u;
+        else if (b[v] < c[w]) ++v;
+        else ++w;
+    }
+    var priority = a[u] switch {
+        >= 'a' and <= 'z' => a[u] - 'a' + 1,
+        >= 'A' and <= 'Z' => a[u] - 'A' + 27,
+        _ => throw new InvalidOperationException(),
+    };
+    score += priority;
+}
+
+Console.WriteLine("{0}", score);
