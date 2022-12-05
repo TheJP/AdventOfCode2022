893226bd079cd3322e8448908117f45ac419efd8
TheJP <jp_55555@hotmail.com>
2022-12-05 19:15:36 +0100

    Solved Day 1 Part 1

---
+++ day01.1/Program.cs
@@ -0,0 +1,15 @@
+﻿using var input = new StreamReader(Environment.GetCommandLineArgs()[1]);
+long max = 0;
+long currentSum = 0;
+string? line;
+while ((line = input.ReadLine()) != null)
+{
+    if (string.IsNullOrWhiteSpace(line)) {
+        max = Math.Max(max, currentSum);
+        currentSum = 0;
+    } else {
+        currentSum += long.Parse(line);
+    }
+}
+
+Console.WriteLine("{0}", max);
