e05b9c9a5f89f85a89d0779531fc45bb5f80110f
TheJP <jp_55555@hotmail.com>
2022-12-05 19:16:49 +0100

    Solved Day 1 Part 2

---
+++ day01.2/Program.cs
@@ -0,0 +1,22 @@
+﻿using var input = new StreamReader(Environment.GetCommandLineArgs()[1]);
+var sums = new List<long>();
+long currentSum = 0;
+string? line;
+while ((line = input.ReadLine()) != null)
+{
+    if (string.IsNullOrWhiteSpace(line))
+    {
+        sums.Add(currentSum);
+        currentSum = 0;
+    }
+    else
+    {
+        currentSum += long.Parse(line);
+    }
+}
+
+sums.Sort();
+
+var topThree = sums.ToArray()[^3..];
+Console.WriteLine("{0}", string.Join(", ", topThree));
+Console.WriteLine("{0}", topThree.Sum());
