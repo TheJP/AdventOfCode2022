894af7cc9e9876caa971227fc4510b7ed9d1098c
TheJP <jp_55555@hotmail.com>
2022-12-05 19:30:05 +0100

    Solved Day 4 Part 1

---
+++ day04.1/Program.cs
@@ -0,0 +1,27 @@
+﻿using var input = new StreamReader(Environment.GetCommandLineArgs()[1]);
+long score = 0;
+string? line;
+
+(int, int) GetRange(string range)
+{
+    var values = range.Split('-', 2);
+    return (int.Parse(values[0]), int.Parse(values[1]));
+}
+
+while ((line = input.ReadLine()) != null)
+{
+    if (string.IsNullOrWhiteSpace(line)) return;
+    var ranges = line.Split(',', 2).Select(GetRange).ToArray();
+    switch ((ranges[0], ranges[1]))
+    {
+        case var ((a, b), (x, y)) when
+                (a <= x && y <= b) ||
+                (x <= a && b <= y):
+            ++score;
+            break;
+        default:
+            break;
+    }
+}
+
+Console.WriteLine("{0}", score);
