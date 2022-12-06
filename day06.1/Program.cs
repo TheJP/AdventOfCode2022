6a790025a5fdc0ec1107fb23b9a08792b0111d07
TheJP <jp_55555@hotmail.com>
2022-12-06 09:20:43 +0100

    Solved Day 6 Part 1

---
+++ day06.1/Program.cs
@@ -0,0 +1,25 @@
+﻿using var input = new StreamReader(Environment.GetCommandLineArgs()[1]);
+string? line = input.ReadLine();
+
+if (line == null) throw new InvalidOperationException();
+
+bool IsStart(string four) {
+    for (int i = 0; i < four.Length - 1; ++i) {
+        for (int j = i + 1; j < four.Length; ++j) {
+            if (four[i] == four[j]) return false;
+        }
+    }
+    return true;
+}
+
+int index = 0;
+do
+{
+    var four = line[index..(index + 4)];
+    if (IsStart(four)) {
+        break;
+    }
+    ++index;
+} while (true);
+
+Console.WriteLine("{0}", index + 4);
