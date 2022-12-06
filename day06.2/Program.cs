b128dea496a76e98b26439d371b520cc8bf80a57
TheJP <jp_55555@hotmail.com>
2022-12-06 09:23:11 +0100

    Solved Day 6 Part 2

---
+++ day06.2/Program.cs
@@ -0,0 +1,26 @@
+﻿using var input = new StreamReader(Environment.GetCommandLineArgs()[1]);
+string? line = input.ReadLine();
+
+if (line == null) throw new InvalidOperationException();
+
+// Using chunk.Distinct() would have been simpler.
+bool IsStart(string chunk) {
+    for (int i = 0; i < chunk.Length - 1; ++i) {
+        for (int j = i + 1; j < chunk.Length; ++j) {
+            if (chunk[i] == chunk[j]) return false;
+        }
+    }
+    return true;
+}
+
+int index = 0;
+do
+{
+    var chunk = line[index..(index + 14)];
+    if (IsStart(chunk)) {
+        break;
+    }
+    ++index;
+} while (true);
+
+Console.WriteLine("{0}", index + 14);
