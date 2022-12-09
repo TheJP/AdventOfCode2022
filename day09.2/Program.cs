767870af57d913910010d164a2b7439356cda498
TheJP <jp_55555@hotmail.com>
2022-12-09 06:37:37 +0100

    Solved Day 9 Part 2

---
+++ day09.2/Program.cs
@@ -0,0 +1,55 @@
+﻿var input = File.ReadLines(Environment.GetCommandLineArgs()[1]);
+
+var knots = Enumerable.Range(0, 10).Select(_ => new Position { X = 0, Y = 0 }).ToArray();
+
+var positions = new HashSet<(int, int)>();
+
+foreach (var line in input)
+{
+    var direction = line[0];
+    var count = int.Parse(line.Split()[^1]);
+
+    for (int i = 0; i < count; ++i)
+    {
+        switch (direction)
+        {
+            case 'L':
+                --knots[0].X;
+                break;
+            case 'U':
+                --knots[0].Y;
+                break;
+            case 'R':
+                ++knots[0].X;
+                break;
+            case 'D':
+                ++knots[0].Y;
+                break;
+            default:
+                throw new InvalidOperationException();
+        }
+
+        for (int t = 1; t < knots.Length; ++t)
+        {
+            var h = knots[t - 1].X - knots[t].X;
+            var v = knots[t - 1].Y - knots[t].Y;
+            if (Math.Abs(h) > 0 && Math.Abs(v) > 0 && Math.Abs(h) + Math.Abs(v) >= 3)
+            {
+                knots[t].X += h / Math.Abs(h);
+                knots[t].Y += v / Math.Abs(v);
+            }
+            else if (Math.Abs(h) > 1)
+            {
+                knots[t].X += h / Math.Abs(h);
+            }
+            else if (Math.Abs(v) > 1)
+            {
+                knots[t].Y += v / Math.Abs(v);
+            }
+        }
+
+        positions.Add((knots[9].X, knots[9].Y));
+    }
+}
+
+Console.WriteLine("{0}", positions.Count);
