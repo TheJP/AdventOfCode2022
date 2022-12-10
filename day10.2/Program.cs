6eb2635e01f9369c8d6f6b0a1cca503dfa194b75
TheJP <jp_55555@hotmail.com>
2022-12-10 06:33:42 +0100

    Solved Day 10 Part 2

---
+++ day10.2/Program.cs
@@ -0,0 +1,45 @@
+﻿var input = File.ReadLines(Environment.GetCommandLineArgs()[1]);
+
+int x = 1;
+int cycles = 0;
+
+var xs = new List<int>();
+xs.Add(x);
+
+foreach (var line in input)
+{
+    if (line == "noop")
+    {
+        ++cycles;
+        xs.Add(x);
+        continue;
+    }
+    if (!line.StartsWith("addx")) throw new InvalidOperationException();
+
+    xs.Add(x);
+
+    var add = int.Parse(line.Split()[^1]);
+    x += add;
+    cycles += 2;
+
+    xs.Add(x);
+}
+
+int column = 0;
+for (int i = 0; i < cycles; ++i)
+{
+    x = xs[i];
+    if (x - 1 <= column && column <= x + 1)
+    {
+        Console.Write("#");
+    }
+    else
+    {
+        Console.Write(".");
+    }
+
+    column = (column + 1) % 40;
+    if (column == 0) {
+        Console.WriteLine();
+    }
+}
